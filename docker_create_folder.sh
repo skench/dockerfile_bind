19cad6f8f31c452eb3d62e9e4fec4058ce8723d0
skench jyg <skench@list.ru>
2018-12-23 09:51:56 +0400

    moding docker_create_folder

--- docker_create_folder.sh
+++ docker_create_folder.sh
@@ -4,3 +4,17 @@ mkdir -p /var/cache/bind/
 mkdir -p /var/log/bind/
 chown -R bind:bind /var/cache/bind/
 chown -R bind:bind /var/log/bind/
+
+echo "logging {" >> /etc/bind/named.conf
+echo "	channel bind.log {" >> /etc/bind/named.conf
+echo "	file \"/var/log/bind/bind.log\";" >> /etc/bind/named.conf
+echo "	severity notice;" >> /etc/bind/named.conf
+echo "	print-category yes;" >> /etc/bind/named.conf
+echo "	print-severity yes;" >> /etc/bind/named.conf
+echo "	print-time yes;" >> /etc/bind/named.conf
+echo "  };" >> /etc/bind/named.conf
+echo "	" >> /etc/bind/named.conf
+echo "	category queries { bind.log; };" >> /etc/bind/named.conf
+echo "	category default { bind.log; };" >> /etc/bind/named.conf
+echo "	category config { bind.log; };" >> /etc/bind/named.conf
+echo "  };" >> /etc/bind/named.conf
